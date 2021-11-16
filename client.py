13059f56b3f52252a8b6f662e04a7cc24fb68f90
khaled3ttia <khaled.3ttia@gmail.com>
2021-11-16 01:04:46 -0600

    server and client apps

---
+++ client.py
@@ -0,0 +1,86 @@
+
+import socket # for the sockets
+import sys # for the command line arguments
+
+# Method to call when the user has not provided the correct
+# number of argument
+def usage():
+    print("Insufficient number of arguments")
+    print(f"{sys.argv[0]} Usage:");
+    print(f"{sys.argv[0]} <server_ip> <server_port>");
+    exit()
+
+
+def readFile(filename):
+    lines = []
+    with open(filename, 'r') as f:
+        lines = f.readlines()
+    return lines
+
+def send(msg):
+    # for each message, we send two messages:
+    #       1- The length of the message (32 bytes string)
+    #       2- The actual message (of the specified length)
+    
+    # all socket send and receive expect bytes, so encode it 
+    message = msg.encode('utf-8')
+
+    # find the length of the message and convert it to a string
+    messageLenStr = str(len(message)).encode('utf-8')
+
+    # if the string representing the length of the message is 
+    # smaller than the specified header size (32 bytes), add 
+    # padding of empty spaces as bytes
+    messageLenStr += b' ' * (HEADER - len(messageLenStr))
+
+    # send the string representing the message length
+    s.send(messageLenStr)
+
+    # send the actual message
+    s.send(message)
+
+    # wait for ACK
+    reply = s.recv(32)
+
+    # if we get a valid ACK , print it 
+    if reply:
+        print(reply.decode('utf-8'))
+
+if __name__ == '__main__':
+    
+    # Make sure that the user has provided the server IP and port number
+    if (len(sys.argv) != 3):
+        usage()
+
+    # Parse command line arguments (server ip and port)
+    HOST = socket.gethostbyname(sys.argv[1])
+    PORT = int(sys.argv[2])
+
+    # Specify the header size (the size of the length string in bytes)
+    HEADER = 32
+
+    # Specify a message that indicates the end of the stream, so that
+    # when the server receives it, it closes this connection with this 
+    # client
+    FIN_MESSAGE = "BYE!"
+    
+    # Server address tuple: ip and port 
+    SERVER = (HOST, PORT)
+
+    # Read the input file 
+    lines = readFile('input.txt')
+
+    # create an IPv4 TCP socket
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    # connect to server using the socket
+    s.connect(SERVER)
+
+    # send each line in the file
+    for line in lines:
+        send(line)
+
+    # at the end, send the FIN_MESSAGE
+    send(FIN_MESSAGE)
+
+
---
+++ server.py
@@ -0,0 +1,88 @@
+import socket
+import sys
+
+if __name__ == '__main__':
+    # Host IP address is my ip address
+    HOST = socket.gethostbyname(socket.gethostname())
+
+    # If the user has provided a port number, use it 
+    # otherwise, use a default port number of 2048
+    PORT = 2048
+    
+    if (len(sys.argv) > 1):
+        try:
+            PORT = int(sys.argv[1])
+        except:
+            print("Invalid Port Number!, using a default 2048")
+            PORT = 2048
+
+    
+    # If the user has provided the output file name
+    # use it, otherwise use 'output.txt' as the output file
+    outfileName = 'output.txt'
+    if (len(sys.argv) > 2):
+        try:
+            outfileName = sys.argv[2]
+        except:
+            print("Invalid output filename, using the default")
+
+
+    #how many bytes to represent the length of message
+    #as a string
+    HEADER = 32
+
+    # Server address tuple: ip and port
+    ADDR =(HOST, PORT)
+
+    # Specify a message that indicates the end of the stream from the
+    # client side. If this message is received by the server, the server
+    # closes the connection with this client 
+    FIN_MESSAGE = "BYE!"
+
+    # Create and IPv4 TCP connection
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    
+    # Bind it 
+    s.bind(ADDR)
+
+    # Wait for a client to connect
+    s.listen() 
+
+    print(f"Server is listening on {HOST}:{PORT}")
+
+    # A counter to count how many messages were received by the server
+    totalReceived = 0
+
+    while True:
+        # accept a connection from a client
+        conn, addr = s.accept()
+        print(f"Client {addr[0]}:{addr[1]} connected")
+        connected = True
+
+        # Open a file for writing
+        with open(outfileName, 'a') as f: 
+            while connected :
+                # The first thing to receive is the message length
+                dataLen = conn.recv(HEADER).decode('utf-8')
+                if dataLen :
+                    dataLen = int(dataLen)
+
+                    # Then receive the actual message
+                    msg = conn.recv(dataLen).decode('utf-8')
+                    print(f"[{addr[0]}:{addr[1]}] {msg}")
+
+                    # If the client sent a FIN message, close the
+                    # client socket
+                    if (msg == FIN_MESSAGE) :
+                        connected = False 
+                    else :
+                    # Otherwise, increment the count of received messages
+                        totalReceived += 1 
+                    # Send an ACK to the client
+                        conn.sendall(bytes(f"ACK[{totalReceived}]", 'utf-8'))
+                    # And write the message to output.txt
+                        f.write(msg)
+                        f.write('\n')
+            conn.close()
+
+
